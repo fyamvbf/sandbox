09b538df0d0bb5acad9f6885ae0ee439d6b1385e
fyamvbf <yamamoto.fumihiko@tis.co.jp>
2021-08-30 22:53:13 +0900

    文字コード指定、情報表示内容追加

--- jibDockerExample/src/main/java/HelloWorld.java
+++ jibDockerExample/src/main/java/HelloWorld.java
@@ -3,11 +3,20 @@ import java.util.Map;
 
 public class HelloWorld {
     public static void main(String[] args)throws Exception{
+    	//固定文言
         System.out.println("Hello");
         System.out.println("Host:" + InetAddress.getLocalHost());
-        Map env = System.getenv();
+        //引数出力
+    	System.out.println("引数:");
+    	for (int i = 0; i < args.length; i++) {
+    		System.out.println(i + ":" + args[i]);
+		}
+    	//環境変数出力
+        System.out.println("環境変数:");
+        Map<String, String> env = System.getenv();
         env.forEach((k,v) ->{
             System.out.println(k + "=" + v);
         });
+
     }
 }
